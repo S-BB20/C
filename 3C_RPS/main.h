9a17db69fde90bffcf67682a8ac48f46c356b512
S-BB20 <77054901+S-BB20@users.noreply.github.com>
2021-05-03 16:23:12 +0900

    Rename main.h to 3C_RPS/main.h

---
+++ 3C_RPS/main.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h> //rand, strcmp 함수를 사용
+#include<time.h> //time 함수를 사용
+#include<windows.h> //system 함수를 사용
+#include<stdbool.h>//while(true)함수를 사용
+#include<conio.h> //getch 함수 사용
+#include<string.h> //strcpy 함수 사용#pragma once
